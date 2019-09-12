7f57efab9757d97d0d6b884b7d9c00fd0dd9bc6b
haifeng520 <39614671+haifeng520@users.noreply.github.com>
2019-09-12 10:11:48 +0800

    组件传值 $emit

--- src/components/tabbar/index.vue
+++ src/components/tabbar/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="tabbar">
-    <Tabitme></Tabitme>
+    <Tabitme txt="首页" mark="home" :sel="selected" @changeSelected="changeSeleted">
+      <img slot="normalImg" src="../../assets/ic_tab_home_normal.png" alt="">
+      <img slot="activeImg" src="../../assets/ic_tab_home_active.png" alt="">
+    </Tabitme>
+    <Tabitme txt="影音" mark="audio" :sel="selected" @changeSelected="changeSeleted">
+      <img slot="normalImg" src="../../assets/ic_tab_audio_normal.png" alt="">
+      <img slot="activeImg" src="../../assets/ic_tab_audio_active.png" alt="">
+    </Tabitme>
+    <Tabitme txt="广播" mark="broadcast" :sel="selected" @changeSelected="changeSeleted">
+      <img slot="normalImg" src="../../assets/ic_tab_broadcast_normal.png" alt="">
+      <img slot="activeImg" src="../../assets/ic_tab_broadcast_active.png" alt="">
+    </Tabitme>
+    <Tabitme txt="小组" mark="group" :sel="selected" @changeSelected="changeSeleted">
+      <img slot="normalImg" src="../../assets/ic_tab_group_normal.png" alt="">
+      <img slot="activeImg" src="../../assets/ic_tab_group_active.png" alt="">
+    </Tabitme>
+    <Tabitme txt="我的" mark="mine" :sel="selected" @changeSelected="changeSeleted">
+      <img slot="normalImg" src="../../assets/ic_tab_mine_normal.png" alt="">
+      <img slot="activeImg" src="../../assets/ic_tab_mine_active.png" alt="">
+    </Tabitme>
   </div>
 </template>
 
@@ -13,13 +32,25 @@ export default {
   },
   data () {
     return {
+      selected: 'home'
     }
   },
-
-  methods: {}
+  methods: {
+    changeSeleted(val) {
+      // val 子组件传回的值
+      this.selected = val;
+    }
+  }
 }
 </script>
 
 <style lang='less' scoped>
-
+.tabbar {
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  height: 60px;
+  width:100%;
+  border-top: 1px solid #ccc;
+}
 </style>
--- src/components/tabbar/tabitem.vue
+++ src/components/tabbar/tabitem.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="tabitem">
-    tabitem
+  <div class="tabitem" @click="change">
+    <span v-if="!flag"><slot name="normalImg"></slot></span>
+    <span v-else><slot name="activeImg"></slot></span>
+    <span :class="{active: flag}">{{txt}}</span>
   </div>
 </template>
 
@@ -11,11 +13,36 @@ export default {
     return {
     }
   },
-
-  methods: {}
+  props: ["txt", "mark", "sel"],
+  computed: {
+    flag() {
+      if(this.mark === this.sel) {
+        return true;
+      }
+      return false;
+    }
+  },
+  methods: {
+    change() {
+      this.$emit('changeSelected', this.mark);
+      this.$router.push('/' + this.mark);
+    }
+  }
 }
 </script>
 
 <style lang='less' scoped>
-
+.tabitem {
+  flex: 1;
+  display: flex; // 转换块元素
+  flex-direction: column;  // 垂直排列
+  align-items: center;
+  img {
+    width: 40px;
+    height: 40px;
+  }
+  .active {
+    color: #42bd56;
+  }
+}
 </style>
